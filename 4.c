4cd538b0d6eba246f16b3be66f516f5767b7c276
rohnkhanna <rohnkhanna@users.noreply.github.com>
2018-04-06 23:19:02 +0530

    4 commit

--- 4.c
+++ 4.c
@@ -1,180 +1,107 @@
+//shortest job first non-preemptive
+//p1	0	20
+//p2	5	36
+//p3	13	19
+//p4	17	42
+
+
 #include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<malloc.h>
-#include<time.h>
-#include<pthread.h>
-#include<semaphore.h>
-struct Process {
-	int time,Atime,Btime,id;
-	clock_t arrival;
-	int flag,completed,p;
-	sem_t se;
-	struct Process *next;
-	int priority;
-	int WT;
-};
-int i=0,k=0;
-typedef struct Process node;
-clock_t start;
-float A_TAT=0,A_WT=0;
-node *front=NULL,*rear=NULL,*temp;
-void *processor(node *S) {
-	clock_t count;
-	while(1) {
-		sem_wait(&S->se);
-		if((S->Atime<=(clock()-start)/CLOCKS_PER_SEC && S->p==1)) {
-			S->p=0;
-			count=clock();
+
+main()
+{
+	int timer=0,i=0,bt=0;
+	int process[4]={1,2,3,4};
+	int arrival[4]={0,5,13,17};
+	int burst_time[4]={20,36,19,42};
+	int burst[4]={20,36,19,42};
+	int process_arrived[4]={1};
+	double priority[4]={0};
+	int complete[4]={0};
+	while(1)
+	{
+		int k;
+		for(k=0;k<4;k++)
+		{
+			if(timer>=arrival[k] && burst[k]!=(-1)) 
+			{
+				priority[k]=1+((timer-arrival[k])/burst[k]);
+			}
 		}
-		if(S->flag==1) {
-			printf("\nProcess-%d Running \nTimer :%d",S->id,(clock()-start)/CLOCKS_PER_SEC);
-			S->flag=0;
-			S->arrival=clock();
-		}
-		if((clock()-count)/CLOCKS_PER_SEC==1) {
-			count=clock();
-			printf("\nTimer :%d",(clock()-start)/CLOCKS_PER_SEC);
-			S->time-=1;
-			if(S->time==0) {
-				S->WT=((clock()-start)/CLOCKS_PER_SEC)-S->Btime-S->Atime;
-				A_TAT+=(clock()-start)/CLOCKS_PER_SEC-S->Atime;
-				A_WT+=((clock()-start)/CLOCKS_PER_SEC)-S->Btime-S->Atime;
-				sleep(2);
-				node *start=rear;
-				while(start!=NULL) {
-					if(start->next==S) {
-						start->next=S->next;
-						break;
-					}
-					if(rear==S) {
-						rear=rear->next;
-						break;
-					}
-					start=start->next;
+		int p,q;
+		for(p=0;p<4;p++)
+		{
+			for(q=0;q<4-1-p;q++)
+			{
+				if(priority[q]<priority[q+1] && process_arrived[q]!=0 && process_arrived[q+1]!=0)
+				{
+					double t1=priority[q];
+					priority[q]=priority[q+1];
+					priority[q+1]=t1;
+					int t2=process_arrived[q];
+					process_arrived[q]=process_arrived[q+1];
+					process_arrived[q+1]=t2;
 				}
-				printf("\nProcess-%d Completed ",S->id);
-				if(rear!=NULL){
-					printf("next Process-%d",rear->id);
-				}
-				S->completed=7;
-				if(rear!=NULL){
-					sem_post(&rear->se);
+				else if(priority[q]==priority[q+1]  && process_arrived[q]!=0 && process_arrived[q+1]!=0)
+				{
+					if(burst[q]<burst[q+1])
+					{
+						int t=process_arrived[q];
+						process_arrived[q]=process_arrived[q+1];
+						process_arrived[q+1]=t;
+					}
 				}
 			}
+		}
+		int y=process_arrived[0];
+		bt=burst[y-1];
+		do
+		{
+			//printf("timer : %d",timer);
+			int x;
+			for(x=0;x<4;x++)
+			{
+				if(timer==arrival[x])
+				{		
+					process_arrived[x]=x+1;
+					printf("process %d arrived\n",x);
+				}	
 			}
-					if(S->completed==7) {
+			bt=bt-1;
+			timer=timer+1;
+			if(bt==0)
+			{
+				printf("process %d completed\n",y );
+				complete[y-1]=timer;
+				i=i+1;
+				burst[y-1]=-1;
+				//priority[0]=0;
+				for(x=0;x<4;x++)
+				{
+					priority[x]=0;
+					if(process_arrived[x]!=0)
+					{
+						process_arrived[x]=x+1;
+					}
+				}
+				break;
+			}
+		}while(1);
+		if(i==4)
 			break;
-		}
-	sem_post(&S->se);
+
 	}
-} 
-void spush(node *temp) {
-	int k;
-	node *start=rear;
-	k=temp->priority;
-	k=1+(temp->WT/temp->time);
-	if(rear==NULL) {
-		rear=temp;
-		rear->next=NULL;
+	int turnaround_time[4]={0};
+	int waiting_time[4]={0};
+	for(i=0;i<4;i++)
+	{
+		turnaround_time[i]=complete[i]-arrival[i];
+		waiting_time[i]=turnaround_time[i]-burst_time[i];
 	}
-	else{
-	int t=temp->time;
-	if(start->priority<k){
-		temp->next = rear;
-        rear=temp;
+	printf("process\tarrival_time\tburst_time\tturnaround_time\twaiting_time\n");
+	for(i=0;i<4;i++)
+	{
+		printf("1\t%d\t\t%d\t\t%d\t\t%d\n",arrival[i],burst_time[i],turnaround_time[i],waiting_time[i]);
 	}
-	else if (start->time > t) {
-        	temp->next = rear;
-        	rear=temp;
-    	}
-    else {
-        	while (start->next != NULL && start->next->time< t) {
-            		start = start->next;
-        	}
-        temp->next = start->next;
-        start->next = temp;
-    }
-	}
-}
-void push() {
-	temp=(node *)malloc(sizeof(node));
-	printf("\nEnter Arrival Time of %d Process :",(i+1));
-	scanf("%d",&temp->Atime);
-	printf("Enter Burst Time :");
-	scanf("%d",&temp->time);
-	temp->id=i+1;
-	temp->p=1;
-	temp->flag=1;
-	temp->Btime=temp->time;
-	temp->priority=1+(temp->WT/temp->time);
-	sem_init(&temp->se,0,0);
-	int t=temp->Atime;
-	node *start=front;
-	if (start->Atime > t) {
-        	temp->next = front;
-        	front=temp;
-    	}
-    	else {
-        	while (start->next != NULL && start->next->Atime < t) {
-            		start = start->next;
-        	}
-        temp->next = start->next;
-        start->next = temp;
-    }
-}
-void main() {
-	int n,l=1;
-	pthread_t p[10];
-	printf("\nEnter No.of Processes :");
-	scanf("%d",&n);
-	while(i<n) {
-		if(front==NULL) {
-			front=(node *)malloc(sizeof(node));
-			printf("Enter Arrival Time of %d Process :",(i+1));
-			scanf("%d",&front->Atime);
-			printf("Enter Burst Time :");
-			scanf("%d",&front->time);
-			front->id=i+1;
-			front->flag=1;
-			front->p=1;
-			front->WT=0;
-			front->Btime=front->time;
-			printf("here");
-			front->priority=1+(front->WT/front->time);
-			sem_init(&front->se,0,0);
-			front->next=NULL;
-		}
-		else {
-			push();
-		}
-		i++;
-	}
-	i=0;
-	
-	start=clock();
-	while(i<n) {
-		temp=front;	
-		if(temp->Atime<=0) {
-			printf("Process-%d is Dicarded Due to Incorrect Arrival Time",temp->id);
-			front=temp->next;
-			temp=front;
-			i++;
-		}
-		if(l==1) {
-				l=0;
-				sem_post(&temp->se);
-			}
-		if((clock()-start)/CLOCKS_PER_SEC==temp->Atime) {
-		//	printf("Process-%d is created",temp->id);
-			pthread_create(&p[i],NULL,processor,temp);
-			front=front->next;
-			spush(temp);
-			i++;
-		}
-	}
-	for(i=0;i<n;i++) {
-		pthread_join(p[i],NULL);
-	}
-	printf("\nAverage Waiting Time :%f\nAverage Turn Around Time :%f",(float)A_WT/n,(float)A_TAT/n);
+	printf("average waiting time is %f .",(waiting_time[0]+waiting_time[1]+waiting_time[2]+waiting_time[3])/4.0);
+	printf("average turnaround time is %f .",(turnaround_time[0]+turnaround_time[1]+turnaround_time[2]+turnaround_time[3])/4.0);
 }
